5449214294654aac8951110684d7fd69f04dd1b6
tharun <drakshamtharun@gmail.com>
2024-01-11 12:24:03 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <h2 className="footer--title">Code with ♥ by <a href="https://github.com/draksham" target="_blank" className="footer--link">Draksham Tharun</a></h2>
+        </footer>
+    )
+}
+
+export default Footer
